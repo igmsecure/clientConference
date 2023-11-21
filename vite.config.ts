e065466aaecce0e367406e915c6e11c9d518dadf
Ibragim Dzaurov <100013546+igmsecure@users.noreply.github.com>
2023-11-22 01:48:08 +0500

    change [base url]

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/react-conference-app',
   server: {
     port: 3000,
     proxy: {
