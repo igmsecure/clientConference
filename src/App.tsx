d3013ac86965cbd004a3631eec638efaff22a9fd
Ibragim Dzaurov <100013546+igmsecure@users.noreply.github.com>
2023-11-22 01:52:48 +0500

    add [base url]

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/client-conference-app">
         <div className="App">
           <Routes>
             <Route path="/" element={<Navigate to="/authors" replace />} />
