6f66756301db19eb149ccdd9d98c210371373e4c
Ibragim Dzaurov <100013546+igmsecure@users.noreply.github.com>
2023-11-23 21:10:17 +0500

    chnge [navigate routes]

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./styles/App.css";
 import { BrowserRouter } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
-// import { Route, Routes, Navigate } from "react-router-dom";
-
-
+import { Route, Routes, Navigate } from "react-router-dom";
 
 // Pages
 import HomePage from "./pages/HomePage.tsx";
 import AuthorPage from "./pages/AuthorPage.tsx";
-
 
 
 function App() {
@@ -18,7 +14,7 @@ function App() {
       <BrowserRouter basename="/clientConference">
         <div className="App">
           <Routes>
-            {/* <Route path="/" element={<Navigate to="/authors" replace />} /> */}
+            <Route path="/" element={<Navigate to="/authors" replace />} />
             <Route path="/authors" element={<HomePage />} />
             <Route path="/authors/:id" element={<AuthorPage />} />
           </Routes>
